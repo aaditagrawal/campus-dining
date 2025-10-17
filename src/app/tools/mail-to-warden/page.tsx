cb84b7b1362700967b5c9d2aae9854f68f5dd00a
sherlock <aadit@aaditagrawal.com>
2025-10-18 00:44:55 +0530

    bug fixes (coz i'm bad at typescript sometimes)

--- src/app/tools/mail-to-warden/page.tsx
+++ src/app/tools/mail-to-warden/page.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import hostelsData from "@/data/hostels.json";
 
-type Hostel = typeof hostelsData[0];
+
 
 interface FormData {
   studentName: string;
